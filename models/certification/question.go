9cd9458bcfcf0b239e6796caa9bcfab13948b101
MrLeea <a964581583@hotmail.com>
2021-11-25 14:59:11 +0800

    Add Reply

--- models/certification/question.go
+++ models/certification/question.go
@@ -6,12 +6,12 @@ import (
 )
 
 type Question struct {
-	questionId int    `json:"question_id"`
-	answerA    string `json:"question_A"`
-	answerB    string `json:"question_B"`
-	answerC    string `json:"question_C"`
-	answerD    string `json:"question_D"`
-	correct    string `json:"question_correct"`
+	QuestionId int    `json:"question_id"`
+	AnswerA    string `json:"question_A"`
+	AnswerB    string `json:"question_B"`
+	AnswerC    string `json:"question_C"`
+	AnswerD    string `json:"question_D"`
+	Correct    string `json:"question_correct"`
 }
 
 func Getonequestion() (Question, error) {
@@ -20,14 +20,14 @@ func Getonequestion() (Question, error) {
 	template := "select question_Id from Questions Order by question_Id"
 	stmt, _ := DB.DB().Query(template)
 	stmt.Scan(&num)
-	que.questionId = rand.Intn(num-1) + 1
+	que.QuestionId = rand.Intn(num-1) + 1
 	template = "select answer1,answer2,answer3,answer4,correct from Questions Where question_Id = ?"
-	stmt, err := DB.DB().Query(template, que.questionId)
+	stmt, err := DB.DB().Query(template, que.QuestionId)
 	if err != nil {
 		return que, err
 	}
 	stmt.Next()
-	err = stmt.Scan(&que.answerA, &que.answerB, &que.answerC, &que.answerD, &que.correct)
+	err = stmt.Scan(&que.AnswerA, &que.AnswerB, &que.AnswerC, &que.AnswerD, &que.Correct)
 	if err != nil {
 		return que, err
 	}
