4036771796ddf4eec8f36870c637036bcf23f8e0
MrLeea <64508229+MrLeea-13155bc@users.noreply.github.com>
2021-12-09 14:03:00 +0800

    Update Return Code To ForwardEnd

--- handler/superuserhandler.go
+++ handler/superuserhandler.go
@@ -1,59 +1,70 @@
 package handler
 
 import (
-	"log"
-	UserModels "main/models/user"
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
+	UserModels "main/models/user"
+	Utils "main/utils"
+	"net/http"
 )
+
 //封禁的原因和封禁树洞的id
-type BanedRequest struct{
+type BanedRequest struct {
 	Post_id int
-	Reason string
+	Reason  string
 }
+
 /*
 目前假设管理员id为1
 */
 //管理员对应的handler，用于封禁用户
 // @Summary 管理员封禁用户
 // @Description 管理员封禁用户
-// @Success 200 
+// @Success 200
 // @Accept application/json
 // @Produce application/json
 // @Param body body BanedRequest true "封禁请求体"
 // @Tags 用户相关接口
 // @Router /api/user/banusers [post]
-func BanUsers(c*gin.Context)  {
-	session:=sessions.Default(c)
-	if IssuperUser:=session.Get("userid").(int);IssuperUser!=1{
-		c.JSON(400,gin.H{"msg":"当前用户不具有封禁权限"})
+func BanUsers(c *gin.Context) {
+	session := sessions.Default(c)
+	userId := session.Get("userid").(int)
+	if userId != Utils.SuperUserId {
+		c.JSON(http.StatusInternalServerError, gin.H{"code": Utils.NotSuperUser})
+		return
 	}
-	var banedrequest BanedRequest
-	if err:=c.ShouldBindJSON(&banedrequest);err!=nil{
-		log.Print("绑定json失败")
-	};
-	var message string="用户封禁失败"
+	var bannedRequest BanedRequest
+	if c.ShouldBind(&bannedRequest) != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"code": Utils.BindDefault})
+		return
+	}
 	//通过树洞的id,查找对应的用户
-	_,ok:=UserModels.BanUserByPostid(banedrequest.Post_id,banedrequest.Reason)
-	if ok {
-		message = "用户封禁成功"
-	} else {
-		message = "用户封禁失败"
+	_, ok := UserModels.BanUserByPostid(bannedRequest.Post_id, bannedRequest.Reason)
+	if !ok {
+		c.JSON(http.StatusInternalServerError, gin.H{"code": Utils.BannedDefault})
+		return
 	}
-	c.JSON(200,gin.H{"msg":message})
+	c.JSON(200, gin.H{"code": Utils.BannedSuccess})
 }
+
 // @Summary 展示已经被ban的用户
 // @Description 展示已经被ban的用户
-// @Success 200 
+// @Success 200
 // @Accept application/json
 // @Produce application/json
 // @Tags 用户相关接口
 // @Router /api/user/showbanedlist [get]
-func ShowBannedList(c*gin.Context){
-	session:=sessions.Default(c)
-	if IssuperUser:=session.Get("userid").(int);IssuperUser!=1{
-		c.JSON(400,gin.H{"msg":"当前用户不具有封禁权限"})
+func ShowBannedList(c *gin.Context) {
+	session := sessions.Default(c)
+	userId := session.Get("userid").(int)
+	if userId != Utils.SuperUserId {
+		c.JSON(http.StatusInternalServerError, gin.H{"code": Utils.NotSuperUser})
+		return
 	}
-	banedlist,_:=UserModels.ShowBannedUsers()
-	c.JSON(200,banedlist)
+	bannedList, err := UserModels.ShowBannedUsers()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"code": Utils.DatabaseDefault})
+		return
+	}
+	c.JSON(http.StatusOK, bannedList)
 }
---
+++ utils/flag.go
@@ -0,0 +1,42 @@
+package utils
+
+//一些信息代码
+
+const (
+	SuperUserId     = 1   //管理员UserId
+	NotSuperUser    = 101 //不是管理员
+	BannedSuccess   = 102 //封禁成功
+	BannedDefault   = 103 //封禁失败
+	BindDefault     = 200 //数据解析错误
+	DatabaseDefault = 300 //数据库发生错误
+)
+
+//登录注册相关的代码
+const (
+	LoginSuccess      = 1000 //登录成功
+	PasswordWrong     = 1001 //密码错误
+	UserNameNotExists = 1002 //用户名不存在，登录时会出现
+	UserIdNotFound    = 1003 //用户ID未找到
+	RegisterSuccess   = 1010 //注册成功
+	UserNameIsExists  = 1011 //用户名已存在，注册时会出现
+	UserIconNotFound  = 1021 //用户头像查找错误
+)
+
+//Post相关代码
+const (
+	NotLogin          = 2001 //用户未登录
+	PostNotExists     = 2002 //帖子不存在
+	CreatePostSuccess = 2010 //创建Post成功
+	CreatePostDefault = 2011 //创建Post失败
+	RevisePostSuccess = 2020 //修改Post成功
+	DeletePostSuccess = 2030 //删除Post成功
+	DeletePostDefault = 2031 //删除Post失败
+)
+
+//Comment相关代码
+const (
+	CreateCommentSuccess = 3010 //创建Comment成功
+	ReviseCommentSuccess = 3020 //修改Comment成功
+	DeleteCommentSuccess = 3030 //删除Comment成功
+	DeleteCommentDefault = 3031 //删除Comment失败
+)
